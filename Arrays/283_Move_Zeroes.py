c6336fba96a91ed40b8db58216d5826cc1cbd593
Pragati Bansal <pragatibansal20@gmail.com>
2026-07-08 22:07:14 +0530

    Update 283_Move_Zeroes.py

--- Arrays/283_Move_Zeroes.py
+++ Arrays/283_Move_Zeroes.py
@@ -1,4 +1,4 @@
-LeetCode 283 - Move Zeroes
+""" LeetCode 283 - Move Zeroes
 
 Pattern:
 Builder + Scanner (Two Pointers)
@@ -21,4 +21,12 @@ Algorithm:
 Key Learning:
 Move all non-zero elements to the front while maintaining their relative order, then all zeroes naturally shift to the end.
 """
+class Solution(object):
+    def moveZeroes(self, nums):
+        j=0
+        for i in range(0,len(nums)):
+            if nums[i]!=0:
+                nums[j],nums[i]=nums[i],nums[j]
+                j+=1
+        return nums
   
